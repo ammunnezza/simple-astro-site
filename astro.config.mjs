fd3716cf8ecf3fc3dc385cab0d9f88ec9b6773d7
Matteo <ammunnezza@gmail.com>
2026-04-08 17:21:46 +0200

    test13

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import icon from 'astro-icon';
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: node({
-    mode: "standalone",
-  }),
   integrations: [
     icon(),
   ],
